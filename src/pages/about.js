d270c26cf6a21c82fcedcb19d81e239e6f99682d
farrely-f <farrely.firenza@outlook.com>
2023-10-04 15:42:27 +0700

    part3: gatsby plugins

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const About = () => {
   return (
     <Layout pageTitle={`About Page`}>
       <p>About page rendered using layout component</p>
+      <StaticImage alt="a photo of lake" src="../images/lake.jpg" />
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle={`Home Page`}>
       <p>Home Page</p>
+      <StaticImage
+        alt="pitbull"
+        src="https://images.unsplash.com/photo-1695753579372-498a4a382ec9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+      />
     </Layout>
   )
 }
