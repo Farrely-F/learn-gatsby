4e2fde358f052a91d5fb2f061b108e2ad3296b14
farrely-f <farrely.firenza@outlook.com>
2023-10-04 14:42:47 +0700

    part2: learn gatsby pages and routing system

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+const About = () => {
+  return (
+    <main>
+      <h1>About Gatsby</h1>
+      <p>Hello World!!!</p>
+    </main>
+  )
+}
+
+export const Head = () => (
+  <>
+    <title>About</title>
+    <meta name="description" content="About Page" />
+  </>
+)
+
+export default About
