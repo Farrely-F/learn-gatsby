f81227a3be4d208bf31186c3369df8eeafbd6b4b
Farrely Firenza <132340248+Farrely-F@users.noreply.github.com>
2023-11-30 15:02:17 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Seo from "../components/seo"
 const IndexPage = () => {
   return (
     <Layout pageTitle={`Home Page`}>
-      <p>Home Page</p>
+      <p>Home Page Test</p>
       <StaticImage
         alt="pitbull"
         src="https://images.unsplash.com/photo-1695753579372-498a4a382ec9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
